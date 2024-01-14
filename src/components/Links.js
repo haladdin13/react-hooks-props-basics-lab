6cdc9798348e8b5c10650289ba34acdc75bdab85
Hussein Aladdin <haladdin13@outlook.com>
2024-01-14 09:08:52 -0500

    completed lab

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+function Links(props) {
+    return (
+        <div>
+            <h2>Links</h2>
+            <a href={props.github}>{props.github}</a>
+            <a href={props.linkedin}>{props.linkedin}</a>
+        </div>
+    )
+}
+
+export default Links;
